71c989a345e5f215a9e4ec66079f0340832c24a1
Kira <54922675+Siri-chan@users.noreply.github.com>
2022-03-08 14:43:27 +1100

    fixed typos

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd $HOME
 touch ~/.vimrc
-echo "source ./.vim/.vimrc" >> ~/vimrc
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+echo "source ./.vim/.vimrc" >> ~/.vimrc
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | sh
 nvm install --lts
 git clone https://github.com/Siri-chan/.vim -C ~/.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
