828f88eb05cebb1f53b6a288020d9d3dcdeb3cb7
Kira <54922675+Siri-chan@users.noreply.github.com>
2022-03-09 14:23:14 +1100

    Update and rename setup.sh to setup.zsh

--- setup.zsh
+++ setup.zsh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/zsh
 cd $HOME
 touch ~/.vimrc
 echo "source ./.vim/.vimrc" >> ~/.vimrc
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
+zsh
 nvm install --lts
-git clone https://github.com/Siri-chan/.vim -C ~/.vim
+git clone https://github.com/Siri-chan/.vim --recursive
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'PlugInstall --sync' +'CocInstall ' +qa
 
