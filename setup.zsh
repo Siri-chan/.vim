083d158f28165770dc276328416d5fc1d4b852fa
Kira <54922675+Siri-chan@users.noreply.github.com>
2022-03-23 11:49:18 +1100

    hiving weird issues with nvm, seeing if this will fix

--- setup.zsh
+++ setup.zsh
@@ -5,6 +5,7 @@ echo "source ~/.vim/.vimrc" >> ~/.vimrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh
 zsh
 nvm install --lts
+zsh
 git clone https://github.com/Siri-chan/.vim --recursive
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'PlugInstall --sync' +'CocInstall ' +qa
