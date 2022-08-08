5828e6b3922015b65038780ec413e5aab7c2675a
Kira <54922675+Siri-chan@users.noreply.github.com>
2022-08-09 09:37:31 +1000

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
-cd $HOME
+"cd $HOME || echo "cd is broken" ; exit"
 touch ~/.vimrc
-echo "source ./.vim/.vimrc" >> ~/.vimrc
+echo "source ~/.vim/.vimrc" >> ~/.vimrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 bash
 nvm install --lts
+bash
 git clone https://github.com/Siri-chan/.vim --recursive
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +'PlugInstall --sync' +'CocInstall ' +qa
+which nvim &> /dev/null && printf "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath=&runtimepath\nsource ~/.vimrc" >> ~/.config/nvim/init.vim ; nvim +'PlugInstall --sync' +'CocInstall ' +qa || vim +'PlugInstall --sync' +'CocInstall ' +qa
