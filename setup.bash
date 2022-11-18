683559d16762d6350a720ace951bc86a28000ad2
Kira <54922675+Siri-chan@users.noreply.github.com>
2022-11-18 15:10:07 +1100

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+##REQUIRES NODE PYTHON GIT AND VIM
+
 "cd $HOME || echo "cd is broken" ; exit"
 touch ~/.vimrc
 echo "source ~/.vim/.vimrc" > ~/.vimrc
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.bash_profile
-nvm install --lts
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+#source ~/.bash_profile
+#nvm install --lts
 source ~/.bash_profile
 git clone https://github.com/Siri-chan/.vim --recursive
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
